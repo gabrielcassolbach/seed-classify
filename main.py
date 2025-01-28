080243f7b5c359f3e72282fe549e359ce0a8476c
Gabriel Bach <gabrielcassolbach@gmail.com>
2025-01-27 23:23:10 -0300

    wip for production

--- main.py
+++ main.py
@@ -10,73 +10,70 @@ from gatt_server_manager import BleApplication, MachineProcessAdvertisement, Pro
 from storage_manager import StorageManager
 from time import sleep
 
-ITERATOR = 0
+def get_next_image_number(folder_path):
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+        return 0
+    existing_files = [f for f in os.listdir(folder_path) if f.endswith(".jpg")]
+    if not existing_files:
+        return 0
+    max_number = max([int(f.split(".")[0]) for f in existing_files])
+    return max_number + 1
 
 def seed_processing(modelManager, camera):
-    global ITERATOR
     image = camera.takePicture()
-    # cv2.imwrite("raw_images/raw_image"+str(ITERATOR)+".jpg", image)
-
-    # test if it is needed to convert the image
-    # image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # Converte a imagem para RGB
-    # modelManager.saveImage(image)                   # Debug
     try:
         image = camera.cropImage(image)
-        # cv2.imwrite("cropped/cropped_image"+str(ITERATOR)+".jpg", image)
         results = modelManager.classifyImage(image)
-    except:
-        print("Img unknown")
-        # cv2.imwrite("unknown/unknown_image"+str(ITERATOR)+".jpg", image)
-          # ITERATOR += 1
-        return -1    
-    #     print("Img unknown")
-    #     cv2.imwrite("cropped/unknown_image"+str(ITERATOR)+".jpg", image)
-
-    ITERATOR += 1
-    return results
+    except Exception as e:
+        print(f"Img unknown: {e}")
+        return -1, None
+    return results, image
 
 def classify_seed(modelManager, camera, serialManager):
-    results = seed_processing(modelManager, camera)
+    results, image = seed_processing(modelManager, camera)
     if results == -1:
         return "unknown"
+    
     max_value = 0
     choice = ""
     print(results)
-    for key, value in results.items(): 
-        if(value > max_value):
+    for key, value in results.items():
+        if value > max_value:
             max_value = value
             choice = key
-    
-    if(max_value < 0.5):
+
+    folder = "unknown"
+    if max_value < 0.5:
         serialManager.sendMessage("unknown")
-        return "unknown"
-    elif(choice == "pure"):
+    elif choice == "pure":
+        folder = "regular"
         serialManager.sendMessage("regular")
-        return choice
-    else: 
+    else:
+        folder = "irregular"
         serialManager.sendMessage("irregular")
-        return choice 
+    
+    next_image_number = get_next_image_number("images/"+folder)
+    cv2.imwrite(f"images/{folder}/{next_image_number}.jpg", image)
+    return choice
 
 def main():
     dir_path = None
-    modelfile = None  # Change this to your actual model file
-    modelManager = None 
+    modelfile = None
+    modelManager = None
     camera = None
     data = {
         "pure": 0,
         "silkcut": 0,
-        # "discolored": 0,
         "broken": 0,
         "unknown": 0
     }
-    #          0        1      2        3         4           5 
     state = ["boot", "idle", "sync", "ready", "processing", "saving"]
     it = 0
     detected = False
     ble_thread = None
     serialManager = SerialManager()
     
-    # ble server
     ble_server = BleApplication()
     ble_server.add_service(ProcessService(0))
     ble_server.register()
@@ -87,42 +84,36 @@ def main():
         ble_server.run()
 
     while True:
-        # print(state[it])
-        if(state[it] == "boot"):
+        if state[it] == "boot":
             serialManager.sendMessage("set_idle")
             it = 1
 
-        if(state[it] == "idle"):
+        if state[it] == "idle":
             message = serialManager.receiveMessage()
-            # print(message)
-            if(message == "set_ready"):
+            if message == "set_ready":
                 it = 3
             if ble_thread is None or not ble_thread.is_alive():
                 ble_thread = threading.Thread(target=start_ble_server, daemon=True)
                 ble_thread.start()
 
-        if(state[it] == "ready"):
+        if state[it] == "ready":
             dir_path = os.path.dirname(os.path.realpath(__file__))
-            modelfile = os.path.join(dir_path, "modelfile.eim")  # Change this to your actual model file
-            modelManager = ModelManager(modelfile) 
+            modelfile = os.path.join(dir_path, "modelfile.eim")
+            modelManager = ModelManager(modelfile)
             data = {
                 "pure": 0,
                 "silkcut": 0,
-                # "discolored": 0,
                 "broken": 0,
                 "unknown": 0
             }
-            message = serialManager.receiveMessage() 
-            if(message == "set_idle"):
+            message = serialManager.receiveMessage()
+            if message == "set_idle":
                 it = 1
-            elif(message == "set_processing"):
-                print("set_processing")
+            elif message == "set_processing":
                 detected = True
-                # camera = CameraManager(camera_type="usb", usb_camera_index=0)
                 it = 4
 
-
-        if(state[it] == "processing"):
+        if state[it] == "processing":
             if detected:
                 camera = CameraManager(camera_type="usb", usb_camera_index=0)
                 seed_type = classify_seed(modelManager, camera, serialManager)
@@ -135,9 +126,8 @@ def main():
                 detected = True
             elif message == "set_saving":
                 it = 5
-        
-        if(state[it] == "saving"):
-            # camera.releaseCamera()
+
+        if state[it] == "saving":
             modelManager.releaseRunner()
             storageManager = StorageManager(data)
             storageManager.insert_data()
@@ -146,13 +136,9 @@ def main():
             data = {
                 "pure": 0,
                 "silkcut": 0,
-                # "discolored": 0,
                 "broken": 0,
                 "unknown": 0
             }
-           
 
-    
 if __name__ == "__main__":
    main()
-   
